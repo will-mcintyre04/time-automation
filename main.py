13ace23463722d4fd7a1245c6885873d5cff374a
Will McIntyre <william.d.j.mcintyre@gmail.com>
2023-07-05 14:35:30 -0400

    removed test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Description:
 This script provides a graphical user interface (GUI) for automating the Metalumen time
 study process. It allows the user to perform two main actions: creating a new spreadsheet
 and updating a database with a spreadsheet. The script integrates two separate functionalities
-provided by the following two scripts: make_new_timestudy.py and database_update.py. Test 2123
+provided by the following two scripts: make_new_timestudy.py and database_update.py.
 
 CLICK THE QUESTION MARK BUTTON FOR DOCUMENTATION
 
